85f41fde7eae458cac70e525621b34967899f7d1
ilayda-sahin <satiilaydasahin@gmail.com>
2024-03-08 10:02:43 +0300

    ilk

---
+++ src/odevdeneme/odev.java
@@ -0,0 +1,17 @@
+package odevdeneme;
+
+public class odev {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Bu metot, verilen iki sayının toplamını hesaplar.
+		 * 
+		 * @param sayi1 Toplanacak ilk sayı.
+		 * @param sayi2 Toplanacak ikinci sayı.
+		 * @return İki sayının toplamı.
+		 */
+		
+	}
+
+}
