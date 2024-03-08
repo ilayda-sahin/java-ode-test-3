109d51b377bccd88486b2a08acf8fe3fb386b1e0
ilayda-sahin <satiilaydasahin@gmail.com>
2024-03-08 11:42:43 +0300

    ikinci

--- src/odevdeneme/odev.java
+++ src/odevdeneme/odev.java
@@ -1,9 +1,9 @@
 package odevdeneme;
 
 public class odev {
-	// Bu bir tek satırlık yorumdur.
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		/**
 		 * Bu metot, verilen iki sayının toplamını hesaplar.
 		 * 
@@ -12,25 +12,17 @@ public class odev {
 		 * @return İki sayının toplamı.
 		 */
 		
-		/*
-		Bu bir çok satırlık yorumdur.
-		Burada birden fazla satırda yorum yapabilirsiniz.
-		*/
+		/**
+		 * Bu metot, verilen iki sayının toplamını hesaplar.
+		 * 
+		 * @param sayi1 Toplanacak ilk sayı.
+		 * @param sayi2 Toplanacak ikinci sayı.
+		 * @return İki sayının toplamı.
+		 */
 		
-		/*
-		Bu bir çok satırlık yorumdur.
-		Burada birden fazla satırda yorum yapabilirsiniz.
-		*/
 		
-		// Bu bir tek satırlık yorumdur.
-		// Bu bir tek satırlık yorumdur.
-		// Bu bir tek satırlık yorumdur.
 	}
-	// Bu bir tek satırlık yorumdur.
+	
 	
 
 }
-/*
-Bu bir çok satırlık yorumdur.
-Burada birden fazla satırda yorum yapabilirsiniz.
-*/
