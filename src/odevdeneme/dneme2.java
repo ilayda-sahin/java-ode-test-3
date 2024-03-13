1e6a5a9a31928d1db2bff24f729c43674f63f24e
ilayda-sahin <satiilaydasahin@gmail.com>
2024-03-13 09:21:41 +0300

    deneme

--- src/odevdeneme/dneme2.java
+++ src/odevdeneme/dneme2.java
@@ -2,11 +2,5 @@ package odevdeneme;
 
 public class dneme2 {
 
-	/**
-	 * Bu metot, verilen iki sayının toplamını hesaplar.
-	 * 
-	 * @param sayi1 Toplanacak ilk sayı.
-	 * @param sayi2 Toplanacak ikinci sayı.
-	 * @return İki sayının toplamı.
-	 */
+	//ilayda
 }
--- src/odevdeneme/odev.java
+++ src/odevdeneme/odev.java
@@ -4,23 +4,7 @@ public class odev {
 	
 	public static void main(String[] args) {
 		
-		/**
-		 * Bu metot, verilen iki sayının toplamını hesaplar.
-		 * 
-		 * @param sayi1 Toplanacak ilk sayı.
-		 * @param sayi2 Toplanacak ikinci sayı.
-		 * @return İki sayının toplamı.
-		 */
 		
-		
-		
-		/**
-		 * Bu metot, verilen iki sayının toplamını hesaplar.
-		 * 
-		 * @param sayi1 Toplanacak ilk sayı.
-		 * @param sayi2 Toplanacak ikinci sayı.
-		 * @return İki sayının toplamı.
-		 */
 		
 		
 	}
