2164e196f352a7776a8b01ff129dfe39bc645036
ilayda-sahin <satiilaydasahin@gmail.com>
2024-03-13 12:02:01 +0300

    deneme5

--- src/odevdeneme/odev.java
+++ src/odevdeneme/odev.java
@@ -4,20 +4,24 @@ public class odev {
 	
 	public static void main(String[] args) {
 		
-		/*
-		 * ilayda
-		 */
-		
-		
-		
-		//deneme1
-		
-		
-		
-		/**
-		 *  deneme2
-		 */
-		 
+		 /* Bu da
+        bir yorum bloğudur. */
+     int x = 5; // Bu da bir yorum satırıdır.
+     
+     /*
+      * Bu bir javadoc bloğudur.
+      * İçinde birden fazla satır var.
+      */
+     int y = 10;
+     
+     /*
+      Bu da
+      bir başka
+      javadoc bloğudur.
+      */
+     
+     int z = x + y; // Toplama işlemi
+ 
 		
 		
 	}
